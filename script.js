b834893503ea4fbd35cc09000f2fa1695eeafc48
DAUJAT Baptiste <baptiste.daujat@sfr.fr>
2021-11-05 13:04:58 +0100

    Add cookie

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const popup = document.querySelector('.popup')
 const close = document.querySelector('.close')
 let cookies = document.cookie;
+
 window.onload = function () {
     setTimeout(function () {
         popup.style.display = 'block'
     }, 2000)
 }
 
-
 close.addEventListener('click', () => {
     popup.style.display = "none"
 })
-
