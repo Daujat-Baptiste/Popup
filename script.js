bd5fdef937ba1ad5f80aad62556f3b22faa7f1a6
DAUJAT Baptiste <baptiste.daujat@sfr.fr>
2021-11-05 12:50:57 +0100

    Delete cookie

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
 const popup = document.querySelector('.popup')
 const close = document.querySelector('.close')
 let cookies = document.cookie;
-
-if (cookies == '') {
-    document.cookie = "seen=true; path=/";
-    cookies = document.cookie.split('; ')
-        .find(row => row.startsWith('seen='))
-        .split('=')[1];
+window.onload = function () {
+    setTimeout(function () {
+        popup.style.display = 'block'
+    }, 2000)
 }
 
-if (cookies == 'true') {
-    window.onload = function () {
-        setTimeout(function () {
-            popup.style.display = 'block'
-        }, 2000)
-    }
-    //; max-age=15 temps expiration en sec
-    document.cookie = "seen=false; path=/";
-    cookies = document.cookie.split('; ')
-        .find(row => row.startsWith('seen='))
-        .split('=')[1];
 
-    close.addEventListener('click', () => {
-        popup.style.display = "none"
-    })
-}
+close.addEventListener('click', () => {
+    popup.style.display = "none"
+})
+
